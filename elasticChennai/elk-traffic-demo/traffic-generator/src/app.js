9d3b52a3edeeb8511011f3059802e1cec13a8b0a
Shane Cardoz M <shanecardoz26@gmail.com>
2026-01-31 23:56:18 +0530

    feat:just add the attribute of the UI path

--- elasticChennai/elk-traffic-demo/traffic-generator/src/app.js
+++ elasticChennai/elk-traffic-demo/traffic-generator/src/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
 const trafficRoutes = require("./routes/traffic.routes");
 
 const app = express();
